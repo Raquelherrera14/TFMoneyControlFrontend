6f9ac910c381b5a19b289dc5e8875ccd33498250
Raquel herrera <u20211e430@upc.edu.pe>
2024-11-16 11:42:22 -0500

    v3

--- src/app/componente/comunidad/comunidad.component.ts
+++ src/app/componente/comunidad/comunidad.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-comunidad',
   standalone: true,
-    imports: [
-        RouterLink,
-        NavbarComponent
-    ],
+  imports: [
+    RouterLink,
+    NavbarComponent,
+    MatButton
+  ],
   templateUrl: './comunidad.component.html',
   styleUrl: './comunidad.component.css'
 })
